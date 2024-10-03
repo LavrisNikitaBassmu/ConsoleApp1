18a7d0f38390a6d49142520f4722f1da77993192
Admin <Admin@WIN-0QFN66U68EP>
2024-10-03 16:59:59 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double x = 27.3198;
+        int d;
+
+        // Извлечь дробную часть x
+        double fractionalPart = x - Math.Floor(x);
+
+        // Преобразовать дробную часть в строку
+        string fractionalPartStr = fractionalPart.ToString();
+
+        // Извлечь первый символ дробной части строки
+        char firstDigitChar = fractionalPartStr[2]; // 2, потому что первый символ — «0», а второй — «.»
+
+        // Преобразовать первый символ в целое число
+        d = int.Parse(firstDigitChar.ToString());
+
+        Console.WriteLine("d = " + d); // Выводит: d = 3
+    }
+}
